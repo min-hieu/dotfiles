5d88b4c330f5aafe809316e11b58d5eeb6625fe2
Charlie <53557912+min-hieu@users.noreply.github.com>
2023-05-29 11:48:25 +0900

    Update lsp.lua

--- .config/nvim/lua/charlie/config/lsp.lua
+++ .config/nvim/lua/charlie/config/lsp.lua
@@ -3,23 +3,8 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'sumneko_lua',
-    'rust_analyzer',
+    'lua_ls',
 })
-
--- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
-
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
